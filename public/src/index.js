67a0b28e3e2f62e62fd3320474c0f2b57f117387
“harinaik203” <harinaik9711@gmail.com>
2025-02-10 11:41:37 +0530

    Final Code 2

---
+++ public/src/index.js
@@ -0,0 +1,28 @@
+
+function homeclick(){
+    document.location.href="index.html";
+}
+function aboutclick(){
+    document.location.href="about.html";
+}
+function helloclick(){
+    window.location.href="https://www.linkedin.com/in/hari-naik-52932b314";
+}
+function linkedInclick(){
+    window.location.href="https://www.linkedin.com/in/hari-naik-52932b314";
+}
+function gitclick(){
+    window.location.href="https://github.com/harinaik203";
+}
+function Instaclick(){
+    window.location.href="https://www.instagram.com/hari_naik_1/";
+}
+function resumeopen() {
+    window.open("./docs/Resume_Hari_Naik.pdf");
+}
+function contactclick(){
+    window.location.href = "mailto:harinaik9711@gmail.com";
+}
+function projectclick() {
+    window.location.href = "project.html";
+}
